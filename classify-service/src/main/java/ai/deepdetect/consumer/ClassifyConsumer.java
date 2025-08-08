de2f5943ccd89e49de5f4c530ea3c543bf6b5f6f
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2025-08-08 06:27:45 +0530

    [classify & notification service] performance : added DLQueue and retryable topic

--- classify-service/src/main/java/ai/deepdetect/consumer/ClassifyConsumer.java
+++ classify-service/src/main/java/ai/deepdetect/consumer/ClassifyConsumer.java
@@ -1,7 +1,12 @@
 package ai.deepdetect.consumer;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 import ai.deepdetect.clients.HistoryClient;
@@ -10,7 +15,6 @@ import ai.deepdetect.dto.HistoryResponse;
 import ai.deepdetect.dto.event.ClassifyEvent;
 import ai.deepdetect.dto.event.NotificationEvent;
 import ai.deepdetect.services.ExternalAPIService;
-// import ai.deepdetect.services.HistoryService;
 import ai.deepdetect.services.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,10 +30,17 @@ public class ClassifyConsumer {
     private final HistoryClient historyClient;
 
     @SuppressWarnings("null")
+    @RetryableTopic(
+        attempts = "3",
+        backoff = @Backoff(
+            delay = 2000,
+            maxDelay = 1500   
+        )
+    )
     @KafkaListener(topics = "classification-request-topic", groupId = "classification-request-group")
-    public void consume(ClassifyEvent classifyEvent) {
+    public void consume(ClassifyEvent classifyEvent, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) long offset) {
         try{
-            System.out.println("Consumer consumed : " + classifyEvent.toString());
+            log.info("Consumer consumed : {}, From topic {} and offset was {}" + classifyEvent.toString(), topic, offset);
 
             // REST CALL : Call the python api for actual prediction 
             ExternalAPIResponse apiResponse = externalAPIService.classifyVideo(classifyEvent);
@@ -48,5 +59,10 @@ public class ClassifyConsumer {
         catch(Exception e) {
             System.out.println(e.toString());
         }
+    }
+
+     @DltHandler
+    public void listenDLT(ClassifyEvent classifyEvent, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) long offset) {
+        log.warn("DLT Received : {} , from {} , offset {}", classifyEvent.toString(),topic,offset);
     }
 }
--- notification-service/src/main/java/ai/deepdetect/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/ai/deepdetect/consumer/NotificationConsumer.java
@@ -1,22 +1,50 @@
 package ai.deepdetect.consumer;
 
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 import ai.deepdetect.dto.event.NotificationEvent;
 import ai.deepdetect.services.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NotificationConsumer {
 
     private final EmailService emailService;
 
+    @RetryableTopic(
+        attempts = "3",
+        backoff = @Backoff(
+            delay = 2000,
+            maxDelay = 1500   
+        )
+    )
     @KafkaListener(topics = "notification-request-topic", groupId = "notification-request-group")
-    public void consume(NotificationEvent notificationEvent) {
-        System.out.println("Consumed : " + notificationEvent.toString());
+    public void consume(
+        NotificationEvent notificationEvent,
+        @Header(KafkaHeaders.RECEIVED_TOPIC) String topicName,
+        @Header(KafkaHeaders.OFFSET) String offset) {
+        
+        log.info("Consumed : {} from topic {} and offset was : {}", notificationEvent.toString(), topicName, offset);
 
         emailService.sendEmail(notificationEvent);
     }
+
+    @DltHandler
+    public void dltHandler(
+        NotificationEvent notificationEvent,
+        @Header(KafkaHeaders.RECEIVED_TOPIC) String topicName,
+        @Header(KafkaHeaders.OFFSET) String offset
+    ) {
+        log.info("Consumed : {} from topic {} and offset was : {}", notificationEvent.toString(), topicName, offset);
+    }
+
 }
